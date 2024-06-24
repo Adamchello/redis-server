eff17ae729f75524b2ba6ad5cbd761aaa320abfa
Adamchello <adam.gornas1@gmail.com>
2024-06-24 22:48:31 +0200

    update(tests): updated deserialize and serialize test cases

--- __tests__/utils/deserialize.ts
+++ __tests__/utils/deserialize.ts
@@ -1,154 +1,102 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ERRORS } from '../../src/utils/constants.js'
 import { deserialize } from '../../src/utils/deserialization/index.js'
 
 describe('Deserialize function', () => {
-  describe('Simple string deserialization', () => {
-    test("should throw an error when CRLF isn't passed at the end", () => {
-      const simpleStringWithoutCRLFSuffix = '+OK'
+  describe('handles errors related to missing CRLF', () => {
+    const missingSuffixInputs = [
+      { input: '+OK', error: ERRORS.DESERIALIZE.MISSING_CRLF, type: 'simple string' },
+      { input: '-Error message', error: ERRORS.DESERIALIZE.MISSING_CRLF, type: 'error message' },
+      { input: '$6\r\nfoobar', error: ERRORS.DESERIALIZE.MISSING_CRLF, type: 'bulk string' },
+      { input: ':123', error: ERRORS.DESERIALIZE.MISSING_CRLF, type: 'integer' },
+      {
+        input: '*2\r\n$4\r\necho\r\n$11\r\nhello world',
+        error: ERRORS.DESERIALIZE.MISSING_CRLF,
+        type: 'array',
+      },
+    ]
 
-      expect(() => {
-        deserialize(simpleStringWithoutCRLFSuffix)
-      }).toThrow(ERRORS.DESERIALIZE.MISSING_CRLF)
-    })
+    test.each(missingSuffixInputs)(
+      'should throw error when $type is missing CRLF',
+      ({ input, error }) => {
+        expect(() => deserialize(input)).toThrow(error)
+      },
+    )
+  })
 
-    test('should parse an empty simple string', () => {
-      const result = deserialize('+\r\n')
-      expect(result).toBe('')
-    })
+  describe('handles errors related to invalid format', () => {
+    const invalidFormatInputs = [
+      {
+        input: '$6foobar\r\n',
+        error: ERRORS.BULK_STRING.INVALID_FORMAT,
+        description: 'bulk string has invalid format',
+      },
+      {
+        input: '$\r\n\r\n',
+        error: ERRORS.BULK_STRING.INVALID_FORMAT,
+        description: 'bulk string is missing length and text',
+      },
+      {
+        input: '$12\r\nfoobar\r\n',
+        error: ERRORS.BULK_STRING.INCORRECT_LENGTH,
+        description: 'bulk string has incorrect length',
+      },
+      {
+        input: ':abc\r\n',
+        error: ERRORS.INTEGER.INVALID_NUMBER,
+        description: 'integer is non-numeric value',
+      },
+      {
+        input: '*12\r\n$4\r\necho\r\n$11\r\nhello world\r\n',
+        error: ERRORS.ARRAY.INCORRECT_LENGTH,
+        description: 'array has incorrect length',
+      },
+      {
+        input: '?Test\r\n',
+        error: ERRORS.DESERIALIZE.UNKOWN_PREFIX,
+        description: 'unknown type prefix is passed',
+      },
+    ]
 
-    test('should parse a simple string', () => {
-      const result = deserialize('+OK\r\n')
-      expect(result).toBe('OK')
+    test.each(invalidFormatInputs)('should throw error when $description', ({ input, error }) => {
+      expect(() => deserialize(input)).toThrow(error)
     })
   })
 
-  describe('Error deserialization', () => {
-    test("should throw an error when CRLF isn't passed at the end", () => {
-      const errorWithoutCRLFSuffix = '-Error message'
+  describe('handles deserialization for empty and null values', () => {
+    const emptyFormatInputs = [
+      { input: '+\r\n', expected: '', description: 'an empty simple string' },
+      { input: '-\r\n', expected: '', description: 'an empty error' },
+      { input: '$0\r\n\r\n', expected: '', description: 'an empty bulk string' },
+      { input: '$-1\r\n', expected: null, description: 'a bulk string with length -1' },
+      { input: '*-1\r\n', expected: null, description: 'an array with length -1' },
+    ]
 
-      expect(() => {
-        deserialize(errorWithoutCRLFSuffix)
-      }).toThrow(ERRORS.DESERIALIZE.MISSING_CRLF)
-    })
-
-    test('should parse an empty error', () => {
-      const result = deserialize('-\r\n')
-      expect(result).toBe('')
-    })
-
-    test('should parse a error', () => {
-      const result = deserialize('-Error message\r\n')
-      expect(result).toBe('Error message')
+    test.each(emptyFormatInputs)('parses $description correctly', ({ input, expected }) => {
+      expect(deserialize(input)).toEqual(expected)
     })
   })
 
-  describe('Bulk string deserialization', () => {
-    test("should throw an error when CRLF isn't passed at the end", () => {
-      const bulkStringWithoutCRLFSuffix = '$6\r\nfoobar'
+  describe('handles deserialization for various valid input types', () => {
+    const validInputs = [
+      { input: '+OK\r\n', expected: 'OK', type: 'a simple string' },
+      { input: '-Error message\r\n', expected: 'Error message', type: 'an error message' },
+      { input: '$12\r\nHello, World\r\n', expected: 'Hello, World', type: 'a bulk string' },
+      { input: ':152351\r\n', expected: 152351, type: 'an integer' },
+      {
+        input: '*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n',
+        expected: ['echo', 'hello world'],
+        type: 'an array',
+      },
+      {
+        input: '*3\r\n$12\r\nHello, World\r\n:16\r\n+OK\r\n',
+        expected: ['Hello, World', 16, 'OK'],
+        type: 'an array with mixed types',
+      },
+    ]
 
-      expect(() => {
-        deserialize(bulkStringWithoutCRLFSuffix)
-      }).toThrow(ERRORS.DESERIALIZE.MISSING_CRLF)
+    test.each(validInputs)('parses $type correctly', ({ input, expected }) => {
+      expect(deserialize(input)).toEqual(expected)
     })
-
-    test('should throw an error when bulk string has incorrect format', () => {
-      const bulkStringMissingCRLFAfterTextLength = '$6foobar\r\n'
-      const bulkStringMissingLengthAndText = '$\r\n\r\n'
-
-      expect(() => {
-        deserialize(bulkStringMissingCRLFAfterTextLength)
-      }).toThrow(ERRORS.BULK_STRING.INVALID_FORMAT)
-
-      expect(() => {
-        deserialize(bulkStringMissingLengthAndText)
-      }).toThrow(ERRORS.BULK_STRING.INVALID_FORMAT)
-    })
-
-    test("should throw an error when CRLF isn't between the length and the text", () => {
-      const bulkStringWithIncorrectLength = '$12\r\nfoobar\r\n'
-
-      expect(() => {
-        deserialize(bulkStringWithIncorrectLength)
-      }).toThrow(ERRORS.BULK_STRING.INCORRECT_LENGTH)
-    })
-
-    test('should return null for bulk string with passed length -1', () => {
-      const result = deserialize('$-1\r\n')
-      expect(result).toBe(null)
-    })
-
-    test('should parse an empty bulk string', () => {
-      const result = deserialize('$0\r\n\r\n')
-      expect(result).toBe('')
-    })
-
-    test('should parse a bulk string', () => {
-      const result = deserialize('$12\r\nHello, World\r\n')
-      expect(result).toBe('Hello, World')
-    })
-  })
-
-  describe('Integer deserialization', () => {
-    test("should throw an error when CRLF isn't passed at the end", () => {
-      const integerWithoutCRLFSuffix = ':123'
-
-      expect(() => {
-        deserialize(integerWithoutCRLFSuffix)
-      }).toThrow(ERRORS.DESERIALIZE.MISSING_CRLF)
-    })
-
-    test("should throw an error when input isn't number", () => {
-      const nonNumericIntegerValue = ':abc\r\n'
-
-      expect(() => {
-        deserialize(nonNumericIntegerValue)
-      }).toThrow(ERRORS.INTEGER.INVALID_NUMBER)
-    })
-
-    test('should parse a number', () => {
-      const result = deserialize(':152351\r\n')
-      expect(result).toBe(152351)
-    })
-  })
-
-  describe('Array deserialization', () => {
-    test("should throw an error when CRLF isn't passed at the end", () => {
-      const arrayWithoutCRLFSuffix = '*2\r\n$4\r\necho\r\n$11\r\nhello world'
-
-      expect(() => {
-        deserialize(arrayWithoutCRLFSuffix)
-      }).toThrow(ERRORS.DESERIALIZE.MISSING_CRLF)
-    })
-
-    test('should throw an error when array length is incorrect', () => {
-      const arrayWithIncorrectLength = '*12\r\n$4\r\necho\r\n$11\r\nhello world\r\n'
-
-      expect(() => {
-        deserialize(arrayWithIncorrectLength)
-      }).toThrow(ERRORS.ARRAY.INCORRECT_LENGTH)
-    })
-
-    test('should return null for array with passed length -1', () => {
-      const result = deserialize('*-1\r\n')
-      expect(result).toBe(null)
-    })
-
-    test('should parse an array', () => {
-      const result = deserialize('*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n')
-      expect(result).toEqual(['echo', 'hello world'])
-    })
-
-    test('should parse an array with mixed elements types', () => {
-      const result = deserialize('*3\r\n$12\r\nHello, World\r\n:16\r\n+OK\r\n')
-      expect(result).toEqual(['Hello, World', 16, 'OK'])
-    })
-  })
-
-  test('should throw an error when unknown type is passed', () => {
-    const unkownPrefixInput = '?Test\r\n'
-
-    expect(() => {
-      deserialize(unkownPrefixInput)
-    }).toThrow(ERRORS.DESERIALIZE.UNKOWN_PREFIX)
   })
 })
