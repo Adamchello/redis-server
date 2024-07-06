e1dd3872a388d8d6d75583be6db4a01cbf721b09
Adamchello <adam.gornas1@gmail.com>
2024-07-06 11:55:20 +0200

    fix(set): added early return pattern to set command

--- src/commands/set.ts
+++ src/commands/set.ts
@@ -4,28 +4,30 @@ import { RedisStore } from '../services/RedisStore.js'
 import { AVAILABLE_EXPIRY_TYPES, getExpiryTimeInMilliseconds } from '../utils/expiration.js'
 
 export function handleSet(args: string[], store: RedisStore) {
+  if (args.length !== 2 && args.length !== 4) {
+    return serialize(ERRORS.COMMAND.MISSING_SET_ARGUMENTS, 'error')
+  }
+  
   if (args.length === 2) {
     const [key, value] = args
     store.set(key, value)
 
     return serialize('OK', 'simpleString')
-  } else if (args.length === 4) {
-    const [key, value, expiryType, expiryTimeString] = args
+  } 
 
-    if (!AVAILABLE_EXPIRY_TYPES.includes(expiryType)) {
-      return serialize(ERRORS.COMMAND.INVALID_SET_EXPIRY_TYPE, 'error')
-    }
+  const [key, value, expiryType, expiryTimeString] = args
 
-    const expiryTimeValue = Number(expiryTimeString)
-    if (Number.isNaN(expiryTimeValue)) {
-      return serialize(ERRORS.COMMAND.INVALID_SET_EXPIRY_VALUE, 'error')
-    }
-
-    const expiryTimestamp = getExpiryTimeInMilliseconds(expiryType, expiryTimeValue)
-    store.set(key, value, expiryTimestamp)
-
-    return serialize('OK', 'simpleString')
+  if (!AVAILABLE_EXPIRY_TYPES.includes(expiryType)) {
+    return serialize(ERRORS.COMMAND.INVALID_SET_EXPIRY_TYPE, 'error')
   }
 
-  return serialize(ERRORS.COMMAND.MISSING_SET_ARGUMENTS, 'error')
+  const expiryTimeValue = Number(expiryTimeString)
+  if (Number.isNaN(expiryTimeValue)) {
+    return serialize(ERRORS.COMMAND.INVALID_SET_EXPIRY_VALUE, 'error')
+  }
+
+  const expiryTimestamp = getExpiryTimeInMilliseconds(expiryType, expiryTimeValue)
+  store.set(key, value, expiryTimestamp)
+
+  return serialize('OK', 'simpleString')
 }
